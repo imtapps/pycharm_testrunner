3ca2c0e7a28c4a28a5179484da3ce513af167b5a
jessica-thatcher <jessica.thatcher@imtapps.com>
2017-05-11 08:54:17 -0500

    Version: Dashes

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="pycharm_testrunner",
+    name="pycharm-testrunner",
     description="pycharm testrunner",
     version="0.0.1",
     packages=find_packages(),
